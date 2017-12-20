80bd6b2bac0e59384b5a33c001f9c94930ad917b
Hugo Vila <hvila3@gmail.com>
2017-12-20 16:55:23 +0100

    fix: fix some mistypes

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 
-import '.App.css';
-import logo from './icons/logo.svg';
-
 import * as BooksAPI from './BooksAPI';
+
+import './App.css';
+import logo from './icons/logo.svg';
 
 import ListBooks from './ListBooks';
 
@@ -12,7 +12,14 @@ import ListBooks from './ListBooks';
 
 class App extends Component {
 
-
+    render() {
+    
+      return (
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      )
+    }
 }
 
 export default App;
