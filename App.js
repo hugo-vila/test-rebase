6287a021a53c3550fe12dbfa689e939bf5c33d75
Hugo Vila <hvila3@gmail.com>
2017-12-20 16:51:08 +0100

    style: sort import dependencies calls

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 
+import '.App.css';
+import logo from './icons/logo.svg';
+
 import * as BooksAPI from './BooksAPI';
-import logo from './icons/logo.svg'
 
 import ListBooks from './ListBooks';
-import '.App.css';
+
 
 
 class App extends Component {
 
 
 }
+
+export default App;
 
