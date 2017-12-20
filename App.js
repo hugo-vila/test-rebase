3017e687d4ea004376b88887c9b96b8aee10e4bf
Hugo Vila <git@hugovila.nom.es>
2017-12-20 12:17:16 +0100

    chore: Add some package

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
 import React, { Component } from 'react';
+import '.App.css'
+import logo from './icons/logo.svg'
 import { Route, Link } from 'react-router-dom';
-
+import * as BooksAPI from './BooksAPI'
